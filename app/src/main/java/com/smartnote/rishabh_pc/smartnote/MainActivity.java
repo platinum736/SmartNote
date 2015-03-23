d3383fcb1b7ef0ca2af57cff762753ed6f724ddb
Rishabh Pandey <platinum_736@yahoo.com>
2015-03-22 21:06:40 -0700

    Adding the onClick Listener

--- app/src/main/java/com/smartnote/rishabh_pc/smartnote/MainActivity.java
+++ app/src/main/java/com/smartnote/rishabh_pc/smartnote/MainActivity.java
@@ -2,38 +2,61 @@ package com.smartnote.rishabh_pc.smartnote;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    EditText note;
+    Button recom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        note = (EditText) findViewById(R.id.note);
+        recom = (Button)findViewById(R.id.recom1);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        recom.setOnClickListener(new View.OnClickListener() {
+            URL url;
+            public void onClick(View v) {
+                try{
+                    URL url = new URL("http://54.152.214.89:1337");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                getCloudData cd=new getCloudData();
+                cd.execute(MainActivity.this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+            }
+        });
     }
 }
+       /* private void getCloudData() throws IOException{
+        Log.d("hi", "" + R.id.recom1);
+        URL url = null;
+        String data="";
+        try {
+            url = new URL("http://54.152.214.89:1337");
+            HttpURLConnection conn;
+            conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            note.setText(note.getText()+" "+conn.getHeaderFields());
+            Log.d("Request",""+conn.getHeaderFields());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }*/
+
---
+++ app/src/main/java/com/smartnote/rishabh_pc/smartnote/getCloudData.java
@@ -0,0 +1,58 @@
+package com.smartnote.rishabh_pc.smartnote;
+
+import android.os.AsyncTask;;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Objects;
+
+/**
+ * Created by rishabh-pc on 3/21/2015.
+ */
+public class getCloudData extends AsyncTask<MainActivity,Integer,MainActivity>{
+    String result="";
+    HttpURLConnection conn;
+    protected MainActivity doInBackground(MainActivity... ma) {
+            Log.d("hi", "" + R.id.recom1);
+            URL url = null;
+            String data="";
+
+            try {
+                url = new URL("http://54.152.214.89:1337");
+
+                conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                result=conn.getResponseMessage();
+         //       ma.note.setText(ma.note.getText()+" "+conn.getHeaderFields());
+                Log.d("Request",""+conn.getHeaderFields());
+                Log.d("Response ::",""+conn.getResponseMessage());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return ma[0];
+
+    }
+
+
+    protected void onPostExecute(MainActivity ma) {
+        super.onPostExecute(ma);
+
+        try {
+            Log.d("Response post execute::",""+conn.getResponseMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ma.note.setText(ma.note.getText()+" "+conn.getHeaderFields());
+    }
+}
